c6e0f11a8dd6a8726d1bcb1a702abb3d8b8f0d54
safalagrawal <99533731+safalagrawal@users.noreply.github.com>
2022-02-14 09:40:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,40 @@
+var box;
 
 function setup() {
   createCanvas(400,400);
-  background(51);
   box = createSprite(200,200,30,30);
 
 }
 
 function draw() 
 {
-  if(keyIsDown(RIGHT_ARROW)){
- background("red");
-box.position.x = box.position.x +5;   
-  }
-  
+   background(30);
 
-  if (keyIsDown(LEFT_ARROW)) 
+  if (keyIsDown(RIGHT_ARROW)) 
   {
-    background("blue");
-    box.position.x = box.position.x -5; 
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
   }
  
     if (keyIsDown(UP_ARROW)) 
   {
-    background("yellow");
-    box.position.y = box.position.y -5; 
+    box.position.y = box.position.y - 5;
+   
   }
 
   if (keyIsDown(DOWN_ARROW)) 
   {
-    background("green");
-    box.position.y = box.position.y +5; 
+    box.position.y = box.position.y + 5;
   }
 
-
-  
   drawSprites();
 }
+
+
+
 
